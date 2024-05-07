8c1d48ebdbd812c712f4709f1022ca080978e7cc
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-07 19:29:39 +0100

    Corrected code allocation

--- server.js
+++ server.js
@@ -25,21 +25,16 @@ app.set("layout", "./layouts/layout"); // not at views root
  *************************/
 app.use(static);
 
-// File Not Found Route - must be last route in list
-app.use(async (req, res, next) => {
-  next({ status: 404, message: "Sorry, we appear to have lost that page." });
-});
-
-// Index route
-// app.get("/", function (req, res) {
-//   res.render("index", { title: "Home" });
-// });
-
 // Index route
 app.get("/", baseController.buildHome);
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
 
 /* ***********************
  * Express Error Handler
