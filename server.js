240adc5df5959a4ee65aabade6b8f4432d3d7711
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-07 19:55:50 +0100

    Add middleware

--- server.js
+++ server.js
@@ -24,10 +24,8 @@ app.set("layout", "./layouts/layout"); // not at views root
  * Routes
  *************************/
 app.use(static);
-
 // Index route
-app.get("/", baseController.buildHome);
-
+app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use("/inv", inventoryRoute);
 
