cba923ac661fc511aa49aadcf0e04efff8fc3565
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-10 09:27:12 +0100

    import utilities in the inventoryRoute

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
 const invVehicle = require("../controllers/vehicleController");
+const utilities = require("../utilities");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
