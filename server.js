5422a18fdcefdb8ab2cfff000c83887450154497
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-10 09:28:20 +0100

    Import invController

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const invController = require("./controllers/invController");
 const utilities = require("./utilities");
 /* *************************
  * View Engine and Templates
@@ -28,6 +29,11 @@ app.use(static);
 app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// app.get(
+//   "/type/:classificationId",
+//   utilities.handleErrors(invController.buildByClassificationId)
+// );
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -50,6 +56,21 @@ app.use(async (err, req, res, next) => {
     title: err.status || "Server Error",
     message,
     nav,
+  });
+});
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.buildByClassificationId();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status === 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    grid,
   });
 });
 
