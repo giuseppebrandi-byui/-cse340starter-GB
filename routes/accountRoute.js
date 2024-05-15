fbca1cfcba2e7ddf16cf303f7eb5058fdd6f76cf
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-15 20:40:07 +0100

    Server-Side Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,6 +7,7 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities");
+const regValidate = require("../utilities/account-validation");
 
 /* ***********************************
  * Deliver Login View
@@ -29,6 +30,8 @@ router.get(
  * ******************************** */
 router.post(
   "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
 );
 
