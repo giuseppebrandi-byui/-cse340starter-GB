52e400655c425cbf7367333fce127bb8a9456a64
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-18 22:03:11 +0100

    Edit files

--- routes/classificationRoute.js
+++ routes/classificationRoute.js
@@ -1,13 +1,13 @@
 // Needed Resources
-const express = require("express");
-const router = new express.Router();
-const regValidate = require("../utilities/account-validation");
-const classificationController = require("../controllers/classificationController");
+// const express = require("express");
+// const router = new express.Router();
+// const regValidate = require("../utilities/account-validation");
+// const classificationController = require("../controllers/classificationController");
 
-// Route to add classification
-router.get("/add-classification", classificationController.addClassification);
-// Route
-router.post("/add-classification", regValidate.classificationValidate());
-// Route to vehicles management
-router.get("/", classificationController.displayVehicleManagement);
-module.exports = router;
+// // Route to add classification
+// router.get("/add-classification", classificationController.addClassification);
+// // Route
+// router.post("/add-classification", regValidate.classificationValidate());
+// // Route to vehicles management
+// router.get("/", classificationController.displayVehicleManagement);
+// module.exports = router;
