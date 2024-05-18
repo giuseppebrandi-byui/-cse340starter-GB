37963ea5fc90b3007e56c4261bbb80d1a7195c3e
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-18 23:06:00 +0100

    Change function name

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -14,7 +14,7 @@ router.get("/detail/:carModelId", invVehicle.buildSingleVehicle);
 // Route to add classification
 router.get("/add-classification", classificationController.addClassification);
 // Route
-router.post("/add-classification", regValidate.classificationValidate());
+router.post("/add-classification", regValidate.classificationRules());
 // Route to vehicles management
 router.get("/", classificationController.displayVehicleManagement);
 module.exports = router;
