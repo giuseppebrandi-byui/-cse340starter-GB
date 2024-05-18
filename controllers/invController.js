1d640e9c274188b4924d15a38306934ffb027343
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-18 22:23:35 +0100

    Remove Comment

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,37 +19,4 @@ invCont.buildByClassificationId = async function (req, res, next) {
   });
 };
 
-/* ***************************
- *  Build inventory by classification view
- * ************************** */
-// invCont.displayVehicleManagement = async function (req, res, next) {
-//   let nav = await utilities.getNav();
-//   const links = [
-//     {
-//       url: "/inv/add-classification",
-//       name: "Add new classification",
-//     },
-//     {
-//       url: "/inv/add-inventory",
-//       name: "Add new inventory",
-//     },
-//   ];
-//   const managmentLinks = await utilities.buildManagementLink(links);
-//   res.render("./inventory/management", {
-//     title: "Vehicle Management",
-//     nav,
-//     managmentLinks,
-//   });
-// };
-
-// invCont.addClassification = async function (req, res, next) {
-//   const newForm = await utilities.buildClassificationForm(res.locals);
-//   let nav = await utilities.getNav();
-//   res.render("./inventory/add-classification", {
-//     title: "Add New Classification",
-//     nav,
-//     newForm,
-//   });
-// };
-
 module.exports = invCont;
