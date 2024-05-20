a1da11c1b78b3fa3ec07a1f803ff422c4d725c68
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-20 22:08:23 +0100

    Finalized Add classification

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -11,7 +11,7 @@ vehicleCont.buildSingleVehicle = async function (req, res, next) {
   const data = await vehicleModel.getvehicleById(inv_id);
   const carDetailsGrid = await utilities.buildSingleVehiclePage(data);
   let nav = await utilities.getNav();
-  res.render("./inventory/vehicle", {
+  res.render("inventory/vehicle", {
     title: "Your Car",
     nav,
     carDetailsGrid,
