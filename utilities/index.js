f8903980dead83e8f7e4f2b608a230589baf8c44
giuseppebrandi-byui <bra22041@byui.edu>
2024-05-22 20:41:06 +0100

    Create dropdown

--- utilities/index.js
+++ utilities/index.js
@@ -26,6 +26,25 @@ Util.getNav = async function (req, res, next) {
   return list;
 };
 
+/* ********************************************
+ * Constructs the select element drop-down list
+ ******************************************** */
+// Util.getSelectEl = async function (req, res, next) {
+//   let data = await invModel.getClassifications();
+//   let dropdown = "<select>";
+//   data.rows.forEach((row) => {
+//     dropdown += "<option value='row.classification_id'>";
+//     dropdown +=
+//       '<a href="/inv/type/' +
+//       row.classification_id +
+//       row.classification_name +
+//       "</a>";
+//     dropdown += "</option>";
+//   });
+//   dropdown += "</select>";
+//   return dropdown;
+// };
+
 /* **************************************
  * Build the classification view HTML
  * ************************************ */
@@ -194,6 +213,25 @@ Util.checkLogin = (req, res, next) => {
     req.flash("notice", "Please log in.");
     return res.redirect("/account/login");
   }
+};
+
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications();
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>';
+  classificationList += "<option value=''>Choose a Classification</option>";
+  data.rows.forEach((row) => {
+    classificationList += '<option value="' + row.classification_id + '"';
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected ";
+    }
+    classificationList += ">" + row.classification_name + "</option>";
+  });
+  classificationList += "</select>";
+  return classificationList;
 };
 
 module.exports = Util;
